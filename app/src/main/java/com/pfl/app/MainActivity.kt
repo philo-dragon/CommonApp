b15304205bb60b136e47575ef75dd9c9dc00bb83
panfeilong <pfl@163.com>
2019-04-01 09:23:08 +0800

    1.升级到andoidx

--- app/src/main/java/com/pfl/app/MainActivity.kt
+++ app/src/main/java/com/pfl/app/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.pfl.app
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
